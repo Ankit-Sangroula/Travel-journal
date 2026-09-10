f162c158d7a876df12a17366b73d78ca25f79b2a
Ankit.3k <ankitsangroula2006@gmail.com>
2026-09-10 22:51:53 +0545

    Make a journery card component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,16 +5,16 @@ export default function Hero() {
   return (
     <div>
       <Header />
-      <div className="flex">
-        <img src={mountain} alt="" />
+      <div className="flex p-10 gap-6">
+        <img src={mountain} alt="mountain" className="h-[204px] w-[152px] object-cover rounded-md" />
         <div className="px-5">
-          <div className="justify-content">
-            <span>JAPAN</span>
+          <div className="">
+            <span className="pr-4">JAPAN</span>
             <a href="">View on Google Maps</a>
           </div>
-          <h1>Mount Fuji</h1>
-          <p>12 Jan, 2021-24 Jan, 2021</p>
-          <p>
+          <h1 className="text-3xl font-bold">Mount Fuji</h1>
+          <p className="font-bold mt-5">12 Jan, 2021-24 Jan, 2021</p>
+          <p className="mt-2 leading-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi
             explicabo exercitationem temporibus in. Dolore labore nobis quod ex
             asperiores debitis perferendis corrupti eveniet itaque sequi
